7f02abbb4b0335146962d63dd987748e711c1260
alex <shizofranky@gmail.com>
2024-01-28 14:11:36 +0300

    upgrade class KeyController

--- src/widgets/input-controller/input-controller.ts
+++ src/widgets/input-controller/input-controller.ts
@@ -1,10 +1,38 @@
 
 
-export class InputController {
 
-    
+interface Move {
+    move:{
+        up:boolean ;
+        right:boolean ;
+        down:boolean ;
+        left:boolean ;
+    }
+}
+
+export class InputController implements Move {
+
+    move: { up: boolean; right: boolean; down: boolean; left: boolean; };
+    attack:boolean ;
+
+    update (keys:string[]) {
+
+        this.move.up = keys.includes('w');
+        this.move.right = keys.includes('d');
+        this.move.down = keys.includes('s');
+        this.move.left = keys.includes('a');
+        /* ---------------------------- */ 
+        this.attack = keys.includes('Enter');
+    }
 
     constructor () {
+        this.move = {
+            up:false ,
+            right:false , 
+            down:false ,
+            left:false ,
+        }
 
+        this.attack = false ;
     }
 }
