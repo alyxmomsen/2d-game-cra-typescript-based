f4abbf7dedda78e3f145519aee5fc967e3cf9ce6
alex <shizofranky@gmail.com>
2024-02-19 15:48:58 +0300

    refactor

--- src/entities/toxic-box/toxic-box.ts
+++ src/entities/toxic-box/toxic-box.ts
@@ -3,7 +3,8 @@ import Sprite from "../../widgets/framesetmanagement/sprite";
 import SpriteManager from "../../widgets/framesetmanagement/sprite-manager";
 import { GameObject } from "../game-object/game-object";
 
-import sprite_main from "./../../toxic-box.png"
+// import sprite_main from "./../../toxic-box.png"
+import sprite_flames from './../../sprites/flames/flames.png' ;
 
 export default class ToxicBox extends GameObject {
 
@@ -14,9 +15,12 @@ export default class ToxicBox extends GameObject {
         const rectSize = 50 ;
 
         const spriteImg_theBucket = new Image() ;
-        spriteImg_theBucket.src = sprite_main ;
+        spriteImg_theBucket.src = sprite_flames ;
 
-
+        const frameSrcSize = 600 ;
+        const frameDistance = 690 ;
+        const offsetY = 804 * 5 + 200 ;
+        const offsetX = 0 ;
         super({
             position:randomPosition({posX:{min:0 , max:600} , posY:{min:0 , max:600}}) ,
             dimensions:{width:rectSize , height:rectSize} ,
@@ -26,11 +30,18 @@ export default class ToxicBox extends GameObject {
             spriteManager:new SpriteManager() ,
             sprites:[new Sprite({
                 image:spriteImg_theBucket , 
-                frameSourceDimensions:{width:1000 , height:1000} , 
-                frameRenderingDimensions:{width:1 * 35 , height:2 * 35} ,
+                frameSourceDimensions:{width:1 * frameSrcSize , height:1.5 * frameSrcSize} , 
+                frameRenderingDimensions:{width:1 * 50 , height:2 * 50} ,
                 framePositionDistance:{byX:0 , byY:0} , 
                 framRelativePosition:{x:0 , y:0} ,
-                frameSet:[{x:0 , y:0}]})] ,
+                frameSet:[
+                    Sprite.makeFrame(frameDistance , 0 , offsetX , offsetY) ,
+                    Sprite.makeFrame(frameDistance , 1 , offsetX , offsetY) ,
+                    Sprite.makeFrame(frameDistance , 2 , offsetX , offsetY) ,
+                    Sprite.makeFrame(frameDistance , 3 , offsetX , offsetY) ,
+                    Sprite.makeFrame(frameDistance , 4 , offsetX , offsetY) ,
+                    Sprite.makeFrame(frameDistance , 5 , offsetX , offsetY) ,
+                ]})] ,
         });
     }
 }
